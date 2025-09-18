b437e9d717ca5c50d834051f81ca8b2ab42017e0
Michael Böiers <michael.boiers2@ingka.com>
2025-09-18 13:05:10 +0200

    add make move buttons

--- web-ui/components/game.tsx
+++ web-ui/components/game.tsx
@@ -11,6 +11,9 @@ interface Game {
     scores: Record<string, number>
 }
 
+type GameSymbol = 'ROCK' | 'PAPER' | 'SCISSORS'
+const gameSymbols: GameSymbol[] = ['ROCK', 'PAPER', 'SCISSORS']
+
 export function Game() {
     const [data, setData] = useState<GameAndId | null>(null)
     const createGameCallback: () => void = () => createGame()
@@ -22,12 +25,27 @@ export function Game() {
         return createGameButton
     }
 
-    const {game} = data
+    const {id: gameId, game} = data
     const {players, scores} = game
 
     return <>
         <h2 className="h4">Current Game Stats</h2>
-        {players.length} players, scores: {players.map((player, playerIndex) => <span key={player}>player #{playerIndex+1}: {scores[player] ?? 0} </span>)}
+        Score: {players.map((player, playerIndex) => <span
+        key={player}>player #{playerIndex + 1}: {scores[player] ?? 0} </span>)}
+
+        <div className="d-flex flex-column gap-1">
+            {players.map((player, playerIndex) => <div key={player}>
+                <div>Player {playerIndex + 1}: <div className="d-inline-flex gap-1">{gameSymbols.map(symbol => <Button onClick={() => {
+                    makeMove(gameId, player, symbol).then(game => setData({id: gameId, game}))
+                }}>
+                    {symbol}
+                </Button>)}</div></div>
+            </div>)}
+        </div>
+
+        <pre className="mt-4 text-body-tertiary">
+            {JSON.stringify(game, null, 4)}
+        </pre>
     </>
 }
 
@@ -37,3 +55,13 @@ async function createGame() {
     return await response.json() as GameAndId
 }
 
+async function makeMove(gameId: string, player: string, symbol: GameSymbol) {
+    const url = `http://localhost:8080/rps/games/${gameId}/moves`
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({player, symbol}),
+        headers: {'Content-Type': 'application/json'}
+    })
+    return await response.json() as Game
+
+}
