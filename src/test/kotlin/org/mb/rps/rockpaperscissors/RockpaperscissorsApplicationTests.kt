c9756af251a3808cfa99ecc092fc4b939bee1144
Michael Böiers <michael.boiers2@ingka.com>
2025-09-18 10:20:22 +0200

    set up tests

--- src/test/kotlin/org/mb/rps/rockpaperscissors/RockpaperscissorsApplicationTests.kt
+++ src/test/kotlin/org/mb/rps/rockpaperscissors/RockpaperscissorsApplicationTests.kt
@@ -1,13 +1,44 @@
 package org.mb.rps.rockpaperscissors
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mb.rps.rockpaperscissors.RpsController.GameCreatedResponse
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
 
-@SpringBootTest
+
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class RockpaperscissorsApplicationTests {
+
+    @LocalServerPort
+    private var port: Int? = null
+
+    @Autowired
+    private lateinit var rpsController: RpsController
+
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
 
     @Test
     fun contextLoads() {
+        assertThat(rpsController).isNotNull
+    }
+
+    @Test
+    fun gameIsCreated() {
+        val (id, game) = restTemplate.postForObject(
+                "http://localhost:$port/rps/games",
+                "",
+                GameCreatedResponse::class.java)
+
+        assertThat(game.players).hasSize(2)
+        assertThat(game.moves).isEmpty()
+
+
     }
 
 }
