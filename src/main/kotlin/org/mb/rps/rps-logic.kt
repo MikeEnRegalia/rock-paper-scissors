10f44c916e59457029040892a5e2848d5d991f61
Michael Böiers <michael.boiers2@ingka.com>
2025-09-19 00:28:40 +0200

    refactor

--- src/main/kotlin/org/mb/rps/rps-logic.kt
+++ src/main/kotlin/org/mb/rps/rps-logic.kt
@@ -6,41 +6,6 @@ import org.mb.rps.GameSymbol.*
 enum class GameSymbol { ROCK, PAPER, SCISSORS }
 enum class GameResult { WIN, DRAW, LOSS }
 
-data class Game(
-    val players: List<String> = listOf(),
-    val doneRounds: List<DoneRound> = listOf(),
-    val openRound: OpenRound = OpenRound()
-)
-
-data class DoneRound(val moves: List<Move> = listOf(), val wins: List<Win> = listOf())
-data class OpenRound(val moves: List<Move> = listOf())
-
-data class Move(val by: String, val symbol: GameSymbol)
-data class Win(val winner: String, val loser: String)
-
-fun Game.canMove(player: String) = openRound.moves.none { it.by == player }
-fun Game.makeMove(move: Move): Game {
-    if (openRound.moves.any { it.by == move.by }) throw IllegalStateException()
-
-    val moves = openRound.moves + move
-    if (moves.size < players.size) return copy(openRound = openRound.copy(moves = moves))
-
-    val wins = buildList {
-        for (player in players) for (otherPlayer in players) if (player > otherPlayer) {
-            val symbol = moves.single { it.by == player }.symbol
-            val otherSymbol = moves.single { it.by == otherPlayer }.symbol
-
-            when (computeResult(symbol, otherSymbol)) {
-                DRAW -> continue
-                WIN -> add(Win(player, otherPlayer))
-                LOSS -> add(Win(otherPlayer, player))
-            }
-        }
-    }
-
-    return copy(doneRounds = doneRounds + DoneRound(moves = moves, wins = wins), openRound = OpenRound())
-}
-
 fun computeResult(symbol: GameSymbol, otherSymbol: GameSymbol) = when (symbol) {
     ROCK -> when (otherSymbol) {
         SCISSORS -> WIN
@@ -61,3 +26,38 @@ fun computeResult(symbol: GameSymbol, otherSymbol: GameSymbol) = when (symbol) {
     }
 }
 
+
+data class Game(
+    val players: List<String> = listOf(),
+    val doneRounds: List<DoneRound> = listOf(),
+    val openRound: OpenRound = OpenRound()
+)
+
+data class DoneRound(val moves: List<Move> = listOf(), val wins: List<Win> = listOf())
+data class OpenRound(val moves: List<Move> = listOf())
+
+data class Move(val by: String, val symbol: GameSymbol)
+data class Win(val winner: String, val loser: String)
+
+fun Game.canMove(player: String) = openRound.moves.none { it.by == player }
+fun Game.makeMove(move: Move): Game {
+    if (openRound.moves.any { it.by == move.by }) throw IllegalStateException()
+
+    val newMoves = openRound.moves + move
+    if (newMoves.size < players.size) return copy(openRound = openRound.copy(moves = newMoves))
+
+    val wins = buildList {
+        for (player in players) for (otherPlayer in players) if (player > otherPlayer) {
+            val symbol = newMoves.single { it.by == player }.symbol
+            val otherSymbol = newMoves.single { it.by == otherPlayer }.symbol
+
+            when (computeResult(symbol, otherSymbol)) {
+                DRAW -> continue
+                WIN -> add(Win(player, otherPlayer))
+                LOSS -> add(Win(otherPlayer, player))
+            }
+        }
+    }
+
+    return copy(doneRounds = doneRounds + DoneRound(moves = newMoves, wins = wins), openRound = OpenRound())
+}
