25ae9184079f4a6556bd4e2d75eefb1058a8f65f
Michael Böiers <michael.boiers2@ingka.com>
2025-09-18 16:27:58 +0200

    refactor

--- src/main/kotlin/org/mb/rps/rps-logic.kt
+++ src/main/kotlin/org/mb/rps/rps-logic.kt
@@ -1,8 +1,10 @@
 package org.mb.rps
 
+import org.mb.rps.GameResult.*
 import org.mb.rps.GameSymbol.*
 
 enum class GameSymbol { ROCK, PAPER, SCISSORS }
+enum class GameResult { WIN, DRAW, LOSS }
 
 data class Game(
     val players: List<String> = listOf(),
@@ -13,7 +15,7 @@ data class Round(val moves: List<Move> = listOf(), val wins: List<Win> = listOf(
 data class Move(val by: String, val symbol: GameSymbol)
 data class Win(val winner: String, val loser: String)
 
-fun Game.canMove(player: String) =  rounds.last().moves.none { it.by == player }
+fun Game.canMove(player: String) = rounds.last().moves.none { it.by == player }
 fun Game.makeMove(move: Move): Game {
     val lastRound = rounds.last()
     if (lastRound.moves.any { it.by == move.by }) throw IllegalStateException()
@@ -26,25 +28,29 @@ fun Game.makeMove(move: Move): Game {
             if (player > otherPlayer) {
                 val playerSymbol = moves.single { it.by == player }.symbol
                 val otherPlayerSymbol = moves.single { it.by == otherPlayer }.symbol
-                if (playerSymbol == otherPlayerSymbol) continue
 
-                val playerWon = when (playerSymbol) {
-                    ROCK -> otherPlayerSymbol == SCISSORS
-                    PAPER -> otherPlayerSymbol == ROCK
-                    SCISSORS -> otherPlayerSymbol == PAPER
-                }
-
-                add(
-                    Win(
-                        if (playerWon) player else otherPlayer,
-                        if (playerWon) otherPlayer else player
+                when (val result = computeResult(playerSymbol, otherPlayerSymbol)) {
+                    DRAW -> continue
+                    WIN, LOSS -> add(
+                        Win(
+                            if (result == WIN) player else otherPlayer,
+                            if (result == WIN) otherPlayer else player
+                        )
                     )
-                )
+                }
             }
         }
     }
 
-    val newLastRound = lastRound.copy(moves = moves, wins = wins)
-    return copy(rounds = rounds.dropLast(1) + newLastRound + Round())
+    return copy(rounds = rounds.dropLast(1) + lastRound.copy(moves = moves, wins = wins) + Round())
+}
+
+fun computeResult(playerSymbol: GameSymbol, otherPlayerSymbol: GameSymbol) = when {
+    playerSymbol == otherPlayerSymbol -> DRAW
+    else -> when (playerSymbol) {
+        ROCK -> if (otherPlayerSymbol == SCISSORS) WIN else LOSS
+        PAPER -> if (otherPlayerSymbol == ROCK) WIN else LOSS
+        SCISSORS -> if (otherPlayerSymbol == PAPER) WIN else LOSS
+    }
 }
 
