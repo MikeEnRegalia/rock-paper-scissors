94bcbfad377786af730c18a1350a4d663bde96fe
Michael Böiers <michael.boiers2@ingka.com>
2025-09-18 08:57:18 +0200

    create project

---
+++ src/test/kotlin/org/mb/rps/rockpaperscissors/RockpaperscissorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.mb.rps.rockpaperscissors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RockpaperscissorsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
