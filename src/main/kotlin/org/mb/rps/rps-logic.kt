54696bbd8f6ca64312133b5a317fa19299fca2c6
Michael Böiers <michael.boiers2@ingka.com>
2025-09-21 15:15:20 +0200

    refactor

--- src/main/kotlin/org/mb/rps/rps-logic.kt
+++ src/main/kotlin/org/mb/rps/rps-logic.kt
@@ -27,17 +27,13 @@ data class Move(val player: String, val symbol: GameSymbol)
 data class Win(val winner: String, val loser: String)
 
 fun Match.canMove(player: String) = player in players && currentGame.moves.none { it.player == player }
-fun Match.playerPairings() = players.flatMap { p -> players.filter { it > p }.map { p to it } }
 
 fun Match.makeMove(move: Move): Match {
     if (currentGame.moves.any { it.player == move.player }) throw IllegalStateException()
-    val newMoves = currentGame.moves + move
-
+    val moves = currentGame.moves + move
     return when {
-        newMoves.size < players.size -> copy(currentGame = currentGame.copy(moves = newMoves))
-        else -> copy(
-            playedGames = playedGames + PlayedGame(moves = newMoves, wins = computeWins(newMoves)),
-            currentGame = CurrentGame())
+        moves.size < players.size -> copy(currentGame = currentGame.copy(moves = currentGame.moves + move))
+        else -> copy(playedGames = playedGames + PlayedGame(moves, computeWins(moves)), currentGame = CurrentGame())
     }
 }
 
@@ -54,3 +50,5 @@ private fun Match.computeWins(newMoves: List<Move>): List<Win> {
     }
     return wins
 }
+
+private fun Match.playerPairings() = players.flatMap { p -> players.filter { it > p }.map { p to it } }
