178ab798198cee01687233efb48c8694a840ed2d
Michael Böiers <michael.boiers2@ingka.com>
2025-09-22 09:07:13 +0200

    use synchronized map

--- src/main/kotlin/org/mb/rps/rps-repo.kt
+++ src/main/kotlin/org/mb/rps/rps-repo.kt
@@ -1,20 +1,25 @@
 package org.mb.rps
 
 import org.springframework.stereotype.Service
+import java.util.Collections.synchronizedMap
 
 @Service
 class RpsRepository {
-    private val matches = mutableMapOf<String, Match>()
+    private val matches = synchronizedMap(mutableMapOf<String, Match>())
 
     fun storeNewMatch(match: Match) {
-        if (match.id in matches) throw IllegalArgumentException("match id ${match.id} already exists")
-        matches[match.id] = match
+        synchronized(matches) {
+            if (match.id in matches) throw IllegalArgumentException("match id ${match.id} already exists")
+            matches[match.id] = match
+        }
     }
 
     fun getMatch(id: String) = matches[id]
 
     fun updateMatch(match: Match) {
-        if (match.id !in matches) throw IllegalArgumentException("match id ${match.id} does not exist")
-        matches[match.id] = match
+        synchronized(matches) {
+            if (match.id !in matches) throw IllegalArgumentException("match id ${match.id} does not exist")
+            matches[match.id] = match
+        }
     }
 }
