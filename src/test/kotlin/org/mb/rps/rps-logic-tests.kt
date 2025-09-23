51f50e9508c5ad2736a3c7718e46944ae23d672b
Michael Böiers <michael.boiers2@ingka.com>
2025-09-23 07:03:59 +0200

    add more tests

--- src/test/kotlin/org/mb/rps/rps-logic-tests.kt
+++ src/test/kotlin/org/mb/rps/rps-logic-tests.kt
@@ -19,6 +19,32 @@ class RpsLogicTests {
         expect(DRAW, "$player vs $player is a draw") { player.meets(player) }
     }
 
+    @ParameterizedTest
+    @CsvSource("ROCK,SCISSORS", "SCISSORS,PAPER", "PAPER,ROCK")
+    fun testScoringLogic(player: GameSymbol, opponent: GameSymbol) {
+        val players = listOf("A", "B")
+
+        fun playMatch(vararg moves: Move): List<Int> {
+            var match = Match(players = players)
+            for (move in moves) match = match.play(move)
+            return match.players
+                .map { player -> match.playedGames.sumOf { it.wins.count { it.winner == player }} }
+        }
+
+        expect(listOf(1, 0)) {
+            playMatch(Move(players[0], player), Move(players[1], opponent))
+        }
+
+        expect(listOf(0, 1)) {
+            playMatch(Move(players[1], player), Move(players[0], opponent))
+        }
+
+        expect(listOf(0, 0)) {
+            playMatch(Move(players[0], player), Move(players[1], player))
+        }
+    }
+
+
     @Test
     fun testMatchFundamentals() {
         val player1 = "A"
